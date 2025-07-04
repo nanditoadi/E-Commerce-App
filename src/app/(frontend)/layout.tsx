85a071a3fdb881c2c18ea6daa5a864004172f794
nanditoadi <nanditoadi42@gmail.com>
2025-07-04 16:48:01 +0700

    feat: add tailwind and shadcn

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,3 +1,5 @@
+import "../../styles/globals.css";
+import React from "react";
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props;
 
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default async function HomePage() {
-  return <p>helo</p>;
+  return <p className="text-4xl">helo</p>;
 }
